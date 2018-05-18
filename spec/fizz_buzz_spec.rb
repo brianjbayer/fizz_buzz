2658a23861ba2056ca1ee5792baf012eeacee9d0
Brian Bayer <bbayer@covermymeds.com>
2018-05-18 13:43:10 -0400

    rubocopped fizz_buzz_spec - i do not do 80 char lines in 2018

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -6,29 +6,29 @@ RSpec.describe 'FizzBuzz' do
   let(:div_by_5)  { 20 }
 
   describe '.fizz_buzz_to' do
-    it { expect(FizzBuzz.fizz_buzz_to 16).to eql '1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16' }
-    it { expect(FizzBuzz.fizz_buzz_to  1).to eql '1' }
+    it { expect(FizzBuzz.fizz_buzz_to(16)).to eql '1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16' }
+    it { expect(FizzBuzz.fizz_buzz_to(1)).to eql '1' }
   end
 
   describe '.fizz_buzz' do
     let(:div_by_3_and_5)     { 30 }
     let(:not_div_by_3_and_5) { 11 }
 
-    it { expect(FizzBuzz.fizz_buzz div_by_3          ).to eql 'Fizz' }
-    it { expect(FizzBuzz.fizz_buzz div_by_5          ).to eql 'Buzz' }
-    it { expect(FizzBuzz.fizz_buzz div_by_3_and_5    ).to eql 'FizzBuzz' }
-    it { expect(FizzBuzz.fizz_buzz not_div_by_3_and_5).to eql not_div_by_3_and_5 }
+    it { expect(FizzBuzz.fizz_buzz(div_by_3)).to eql 'Fizz' }
+    it { expect(FizzBuzz.fizz_buzz(div_by_5)).to eql 'Buzz' }
+    it { expect(FizzBuzz.fizz_buzz(div_by_3_and_5)).to eql 'FizzBuzz' }
+    it { expect(FizzBuzz.fizz_buzz(not_div_by_3_and_5)).to eql not_div_by_3_and_5 }
   end
 
   describe '.fizz' do
     let(:not_div_by_3) { 1 }
-    it { expect(FizzBuzz.fizz     div_by_3 ).to eql 'Fizz' }
-    it { expect(FizzBuzz.fizz not_div_by_3 ).to eql '' }
+    it { expect(FizzBuzz.fizz(div_by_3)).to eql 'Fizz' }
+    it { expect(FizzBuzz.fizz(not_div_by_3)).to eql '' }
   end
 
   describe '.buzz' do
     let(:not_div_by_5) { 2 }
-    it { expect(FizzBuzz.buzz     div_by_5 ).to eql 'Buzz' }
-    it { expect(FizzBuzz.buzz not_div_by_5 ).to eql '' }
+    it { expect(FizzBuzz.buzz(div_by_5)).to eql 'Buzz' }
+    it { expect(FizzBuzz.buzz(not_div_by_5)).to eql '' }
   end
 end
